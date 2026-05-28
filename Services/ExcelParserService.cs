fd8754dc51e4e973c77ccaa05faea528eff92321
lenargaynullin <lenar.gaynullin88@gmail.com>
2026-05-28 10:46:11 +0300

    Добавьте файлы проекта.

---
+++ Services/ExcelParserService.cs
@@ -0,0 +1,40 @@
+﻿using OfficeOpenXml;
+
+namespace AsuGenerator.Web.Services;
+
+public class ExcelParserService
+{
+    public VentAvtomatikaConfig ParseVentaFile(Stream fileStream)
+    {
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        var config = new VentAvtomatikaConfig();
+
+        using (var package = new ExcelPackage(fileStream))
+        {
+            var worksheet = package.Workbook.Worksheets[0]; // Берем первый лист
+
+            // Чтение шапки (Заказчик)
+            config.ClientName = worksheet.Cells["B3"].Value?.ToString() ?? "Не указан";
+            config.CompanyName = worksheet.Cells["B4"].Value?.ToString() ?? "Не указана";
+            config.KpNumber = worksheet.Cells["G5"].Value?.ToString() ?? "";
+
+            // Нагреватель водяной (проверка крестика в ячейке A16)
+            if (worksheet.Cells["A16"].Value?.ToString()?.ToUpper() == "X")
+            {
+                config.Heater1Type = "Водяной";
+                double.TryParse(worksheet.Cells["C17"].Value?.ToString(), out var pumpPower);
+                config.Heater1PumpPowerKw = pumpPower;
+            }
+
+            // Вентилятор притока (Мощность в C52)
+            double.TryParse(worksheet.Cells["C52"].Value?.ToString(), out var fanPower);
+            config.SupplyFanPowerKw = fanPower;
+
+            // Тип пуска вентилятора
+            if (worksheet.Cells["E54"].Value?.ToString()?.ToUpper() == "X")
+                config.SupplyFanRegulation = "Частотное";
+        }
+
+        return config;
+    }
+}
---
+++ Services/Models.cs
@@ -0,0 +1,13 @@
+﻿namespace AsuGenerator.Web.Services;
+
+public class VentAvtomatikaConfig
+{
+    public string ClientName { get; set; } = string.Empty;
+    public string CompanyName { get; set; } = string.Empty;
+    public string KpNumber { get; set; } = string.Empty;
+
+    public double SupplyFanPowerKw { get; set; }
+    public string SupplyFanRegulation { get; set; } = "Прямой пуск";
+    public string Heater1Type { get; set; } = "Нет";
+    public double Heater1PumpPowerKw { get; set; }
+}
