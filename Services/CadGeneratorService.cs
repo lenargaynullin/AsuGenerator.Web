4e85485cf764a5e1aebc9b277f2b18a28b413861
lenargaynullin <lenar.gaynullin88@gmail.com>
2026-06-03 16:23:34 +0300

    CadGeneratorService исправил штамп. Вывел XT1 QS1

--- Services/CadGeneratorService.cs
+++ Services/CadGeneratorService.cs
@@ -1,9 +1,10 @@
 ﻿using netDxf;
+using netDxf.Blocks;
+using netDxf.Collections;
 using netDxf.Entities;
 using netDxf.Tables;
-using System.IO;
 using System.Collections.Generic;
-
+using System.IO;
 namespace AsuGenerator.Web.Services;
 
 public class CadGeneratorService
@@ -37,59 +38,44 @@ public class CadGeneratorService
         // Базовый слой "0", который гарантированно есть в любом файле подложки
         Layer defaultLayer = sheet1.Layers["0"];
 
-        // 1. ЧЕРТИМ ВВОДНОЙ АВТОМАТ QF1 (Слева чертежа: X = 50, Y = 210)
+
+
+
+        // XT1
         double currentX = 50;
-        double currentY = 210;
+        double currentY = 250;
 
-        var qf1 = components.Find(c => c.Designation == "QF1");
-        if (qf1 != null)
+        var xt1 = components.Find(c => c.Designation == "XT1");
+        if (xt1 != null)
         {
-            // Вертикальный провод питания сверху (длина 40 мм)
-            DrawVerticalWires(sheet1, currentX, currentY + 40, 40, defaultLayer);
-            // Вставляем блок УГО автомата из вашего файла qf_3p.dxf
-            InsertComponentBlock(sheet1, "QF_3P", new Vector2(currentX, currentY), qf1.Designation, qf1.Article, defaultLayer, gostStyle);
-            // Отвод провода вниз после автомата
-            DrawVerticalWires(sheet1, currentX, currentY - 15, 40, defaultLayer);
+            // Вставляем блок УГО
+            InsertComponentBlock(sheet1, "terminal", new Vector2(currentX, currentY), xt1.Designation, xt1.Article, defaultLayer, gostStyle, 4);
+
+            // Отвод провода вниз
+            DrawVerticalWires(sheet1, currentX, currentY - 4, 20, defaultLayer, 4);
+        }
+        currentY = currentY - 24;
+
+        // QS1
+        var qs1 = components.Find(c => c.Designation == "QS1");
+        if (qs1 != null)
+        {
+            // Вставляем блок УГО
+            InsertComponentBlock(sheet1, "qs_4p", new Vector2(currentX, currentY), qs1.Designation, qs1.Article, defaultLayer, gostStyle, 1);
+
+            // Отвод провода вниз
+            DrawVerticalWires(sheet1, currentX, currentY - 8, 20, defaultLayer, 4);
         }
 
-        // 2. АВТОМАТИЧЕСКАЯ РАССТАНОВКА ФИДЕРОВ ОБОГРЕВА ШУЭ В ЦИКЛЕ С СДВИГОМ ПО X
-        // Считываем реальное количество линий из опросника ШУЭ (дефолт 5, как на вашем скрине)
-        int outletsCount = config.OutletsHeatingCount > 0 ? config.OutletsHeatingCount : 5;
-
-        double startX = 100; // Цепочку фидеров обогрева начинаем правее, с координаты X = 100
-        double stepX = 35;   // Жесткий ГОСТ-шаг сдвига вправо для каждого фидера (35 мм)
-
-        for (int i = 1; i <= outletsCount; i++)
-        {
-            double fX = startX + ((i - 1) * stepX);
-
-            // Предохранитель: останавливаем цикл, чтобы чертеж не залез на область штампа (X_max = 220)
-            if (fX > 220) break;
-
-            // Ищем фидер в сформированной на сайте b2b-спецификации (QFD1, QFD2...)
-            var qfd = components.Find(c => c.Designation == $"QFD{i}");
-            if (qfd != null)
-            {
-                // Проводим вертикальный подвод проводов сверху к фидеру
-                DrawVerticalWires(sheet1, fX, currentY + 40, 40, defaultLayer);
-
-                // Вставляем блок диффавтомата (используем ваш файл qf_3p как графический атом)
-                InsertComponentBlock(sheet1, "QF_3P", new Vector2(fX, currentY), qfd.Designation, qfd.Article, defaultLayer, gostStyle);
-
-                // Проводим отвод провода вниз от автомата к клеммам
-                DrawVerticalWires(sheet1, fX, currentY - 15, 40, defaultLayer);
-
-                // Вставляем силовой выходной клеммник ТЕРМИНАЛ_2 в самом низу (Y = 80)
-                InsertComponentBlock(sheet1, "TERMINAL_2", new Vector2(fX, 80), string.Format("X1.{0}", i), "ЗНИ-4", defaultLayer, gostStyle);
-            }
-        }
 
         schematics.Add(string.Format("Схема_Лист1_Сила_КП{0}.dxf", config.KpNumber), SaveToBytes(sheet1));
 
+        /*
         // --- ЛИСТ 2: АВТОМАТИКА И ПЛК (A3_2.dxf) ---
         DxfDocument sheet2 = _blockManager.GetTemplate("A3_2");
         FillStamp(sheet2, config, isFirstSheet: false, gostStyle);
         schematics.Add(string.Format("Схема_Лист2_Автоматика_КП{0}.dxf", config.KpNumber), SaveToBytes(sheet2));
+        */
 
         return schematics;
     }
@@ -103,17 +89,31 @@ public class CadGeneratorService
 
         if (isFirstSheet)
         {
-            double stampStartX = stampEndX - 185;
-            // Принудительно задаем гостовский b2b-стиль текста для каждой ячейки штампа
-            dxf.Entities.Add(new netDxf.Entities.Text(config.CompanyName.ToUpper(), new Vector2(stampStartX + 5, stampStartY + 5), 5) { Layer = defaultLayer, Style = style });
-            dxf.Entities.Add(new netDxf.Entities.Text($"ЩУВ. КП № {config.KpNumber}", new Vector2(stampStartX + 5, stampStartY + 20), 5) { Layer = defaultLayer, Style = style });
-            dxf.Entities.Add(new netDxf.Entities.Text("Разработал: AsuGenerator SaaS", new Vector2(stampStartX + 5, stampStartY + 45), 3.5) { Layer = defaultLayer, Style = style });
-            dxf.Entities.Add(new netDxf.Entities.Text("1", new Vector2(stampEndX - 25, stampStartY + 15), 3.5) { Layer = defaultLayer, Style = style });
+            double stampStartX = stampEndX - 185; // 230
+            
+
+            dxf.Entities.Add(new netDxf.Entities.Text(config.DocDesignation.ToUpper(), new Vector2(stampStartX + 107, stampStartY + 45), 3.5) { Layer = defaultLayer, Style = style }); // КУВФ.421417.215
+            dxf.Entities.Add(new netDxf.Entities.Text(config.CabinetName, new Vector2(stampStartX + 77, stampStartY + 31), 2.5) { Layer = defaultLayer, Style = style }); // Шкаф управления вентиляцией
+            dxf.Entities.Add(new netDxf.Entities.Text(config.ProjectNumber.ToUpper(), new Vector2(stampStartX + 71, stampStartY + 21), 2.5) { Layer = defaultLayer, Style = style }); // ОВИК-ШУВ1.2-1-8.0-0-0-0-1-20-1.0-00
+            dxf.Entities.Add(new netDxf.Entities.Text(config.CompanyName.ToUpper(), new Vector2(stampStartX + 145, stampStartY + 5), 3.5) { Layer = defaultLayer, Style = style }); // AsuGenerator
+            
+            dxf.Entities.Add(new netDxf.Entities.Text("Гайнуллин Л.Т.", new Vector2(stampStartX + 18, stampStartY + 26), 2) { Layer = defaultLayer, Style = style }); // Разработал
+            dxf.Entities.Add(new netDxf.Entities.Text("Гайнуллин Л.Т.", new Vector2(stampStartX + 18, stampStartY + 21), 2) { Layer = defaultLayer, Style = style }); // Проверил
+            dxf.Entities.Add(new netDxf.Entities.Text("Гайнуллин Л.Т.", new Vector2(stampStartX + 18, stampStartY + 11), 2) { Layer = defaultLayer, Style = style }); // Н. контр.
+            dxf.Entities.Add(new netDxf.Entities.Text("Гайнуллин Л.Т.", new Vector2(stampStartX + 18, stampStartY + 6), 2) { Layer = defaultLayer, Style = style }); // Утвердил
+
+            string currentShortDate = DateTime.Now.ToString("dd.MM.yy");
+            dxf.Entities.Add(new netDxf.Entities.Text(currentShortDate, new Vector2(stampStartX + 55, stampStartY + 26), 2) { Layer = defaultLayer, Style = style }); // дата
+            dxf.Entities.Add(new netDxf.Entities.Text(currentShortDate, new Vector2(stampStartX + 55, stampStartY + 21), 2) { Layer = defaultLayer, Style = style }); // дата
+            dxf.Entities.Add(new netDxf.Entities.Text(currentShortDate, new Vector2(stampStartX + 55, stampStartY + 11), 2) { Layer = defaultLayer, Style = style }); // дата
+            dxf.Entities.Add(new netDxf.Entities.Text(currentShortDate, new Vector2(stampStartX + 55, stampStartY + 6), 2) { Layer = defaultLayer, Style = style }); // дата
+
+            dxf.Entities.Add(new netDxf.Entities.Text("1", new Vector2(stampStartX + 148, stampStartY + 16), 2) { Layer = defaultLayer, Style = style }); // Номер листа
         }
         else
         {
             double smallStampStartX = stampEndX - 110;
-            dxf.Entities.Add(new netDxf.Entities.Text($"ЩУВ. КП № {config.KpNumber}", new Vector2(smallStampStartX + 5, stampStartY + 5), 4) { Layer = defaultLayer, Style = style });
+            dxf.Entities.Add(new netDxf.Entities.Text(config.ProjectNumber.ToUpper(), new Vector2(smallStampStartX + 5, stampStartY + 5), 3.5) { Layer = defaultLayer, Style = style });
             dxf.Entities.Add(new netDxf.Entities.Text("2", new Vector2(stampEndX - 15, stampStartY + 5), 3.5) { Layer = defaultLayer, Style = style });
         }
     }
@@ -122,51 +122,61 @@ public class CadGeneratorService
     // new
 
     // 1. В СИГНАТУРУ МЕТОДА ДОБАВЛЯЕМ ПЕРЕДАЧУ СТИЛЯ (TextStyle style)
-    private void InsertComponentBlock(DxfDocument targetDxf, string blockName, Vector2 position, string designation, string article, Layer layer, TextStyle style)
+    private void InsertComponentBlock(DxfDocument targetDxf, string blockName, Vector2 position, string designation, string article, Layer layer, TextStyle style, int numberOfComponent)
     {
         try
         {
             string safeBlockName = blockName.ToLower().Trim();
-            DxfDocument sourceBlock = _blockManager.GetBlock(safeBlockName);
 
-            // 1. Копируем отрезки (Lines) с их родными слоями из файла блока
-            foreach (var line in sourceBlock.Entities.Lines)
+            // Получаем исходный документ макроса (он содержит примитивы на базовом слое "0")
+            DxfDocument sourceBlockDxf = _blockManager.GetBlock(safeBlockName);
+
+            // 1. Ищем или динамически создаем Определение Блока (Block Definition) в целевом файле
+            Block? targetBlockDefinition = targetDxf.Blocks.FirstOrDefault(b => b.Name.Equals(safeBlockName, StringComparison.OrdinalIgnoreCase));
+
+            if (targetBlockDefinition == null)
             {
-                Vector3 startPos = line.StartPoint + new Vector3(position.X, position.Y, 0);
-                Vector3 endPos = line.EndPoint + new Vector3(position.X, position.Y, 0);
+                // Создаем новое b2b-описание блока
+                targetBlockDefinition = new Block(safeBlockName);
 
-                var newLine = new netDxf.Entities.Line(startPos, endPos);
-                // Если слой есть в исходном файле, netDxf перенесет его сам безопасно
-                if (line.Layer != null) newLine.Layer = line.Layer;
-
-                targetDxf.Entities.Add(newLine);
-            }
-
-            // 2. Копируем окружности (Circles)
-            foreach (var circle in sourceBlock.Entities.Circles)
-            {
-                Vector3 centerPos = circle.Center + new Vector3(position.X, position.Y, 0);
-                var newCircle = new netDxf.Entities.Circle(centerPos, circle.Radius);
-                if (circle.Layer != null) newCircle.Layer = circle.Layer;
-                targetDxf.Entities.Add(newCircle);
-            }
-
-            // 3. Копируем полилинии (Polylines2D) — ФИКС: Убрано жесткое переопределение слоя "0"
-            foreach (var polyline in sourceBlock.Entities.Polylines2D)
-            {
-                var clonePoly = (netDxf.Entities.Polyline2D)polyline.Clone();
-                foreach (var vertex in clonePoly.Vertexes)
+                // Клонируем ВСЕ сущности из исходного файла (Линии, Окружности, Дуги, Тексты) внутрь блока
+                foreach (var entity in sourceBlockDxf.Entities.All)
                 {
-                    vertex.Position = new Vector2(vertex.Position.X + position.X, vertex.Position.Y + position.Y);
+                    // Используем оператор приведения типа as и проверку на null
+                    if (entity.Clone() is netDxf.Entities.EntityObject clone)
+                    {
+                        // Принудительно выставляем слой "0", чтобы свойства наследовались от Insert
+                        clone.Layer = targetDxf.Layers.ElementAt(0);
+                        targetBlockDefinition.Entities.Add(clone);
+                    }
                 }
-                // Оставляем родной слой полилинии из файла, чтобы избежать падения в catch
-                targetDxf.Entities.Add(clonePoly);
+
+                // Регистрируем определение макроса в таблице блоков чертежа
+                targetDxf.Blocks.Add(targetBlockDefinition);
             }
+
+            // 2. ВСТАВЛЯЕМ БЛОК КАК ЕДИНОЕ ЦЕЛОЕ (Insert Reference)
+            // Преобразуем координаты Vector2 в Vector3 (Z = 0)
+            Vector2 insertionPoint = new Vector2(position.X, position.Y);
+
+            for (int i = 0; i < numberOfComponent; i++)
+            {
+                var blockInsertion = new Insert(targetBlockDefinition, insertionPoint)
+                {
+                    Layer = layer // Блок целиком падает на ваш b2b-слой автоматики
+                };
+                targetDxf.Entities.Add(blockInsertion);
+                insertionPoint = new Vector2(insertionPoint.X + 8, insertionPoint.Y);
+            }
+
+
+            
         }
         catch (Exception ex)
         {
-            // Если блок все-таки упал, чертим ГОСТ-прямоугольник на слое "0", чтобы чертеж не остался пустым
-            System.Diagnostics.Debug.WriteLine($"[AsuSaaS]: Ошибка блока {blockName}: {ex.Message}");
+            System.Diagnostics.Debug.WriteLine($"[AsuSaaS CRITICAL]: Ошибка блочной вставки {blockName}: {ex.Message}");
+
+            // Страховочный ГОСТ-прямоугольник на случай отсутствия DXF-файла на диске
             double w = 15; double h = 10;
             targetDxf.Entities.Add(new netDxf.Entities.Line(position, new Vector2(position.X + w, position.Y)) { Layer = layer });
             targetDxf.Entities.Add(new netDxf.Entities.Line(new Vector2(position.X + w, position.Y), new Vector2(position.X + w, position.Y + h)) { Layer = layer });
@@ -174,34 +184,19 @@ public class CadGeneratorService
             targetDxf.Entities.Add(new netDxf.Entities.Line(new Vector2(position.X, position.Y + h), position) { Layer = layer });
         }
 
-        // --- 4. МАРКИРОВКА ШРИФТОМ GOST Type BU ---
+        // --- 3. МАРКИРОВКА ШРИФТОМ GOST Type BU (Вне блока) ---
         var textDes = new netDxf.Entities.Text(designation, new Vector2(position.X - 12, position.Y + 2), 3.5)
         {
             Layer = layer,
             Style = style
         };
         targetDxf.Entities.Add(textDes);
-
-        string shortValue = article.Contains("32А") || article.Contains("32A") ? "32А" :
-                           (article.Contains("16А") || article.Contains("16A") ? "16А" : "25А");
-
-        var textVal = new netDxf.Entities.Text(shortValue, new Vector2(position.X - 12, position.Y - 5), 2.5)
-        {
-            Layer = layer,
-            Style = style
-        };
-        targetDxf.Entities.Add(textVal);
     }
 
-
-
-
-
-
-    private void DrawVerticalWires(DxfDocument dxf, double xStart, double yStart, double length, Layer layer)
+    private void DrawVerticalWires(DxfDocument dxf, double xStart, double yStart, double length, Layer layer, int numberOfLines)
     {
         double phaseStep = 8;
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < numberOfLines; i++)
         {
             double cx = xStart + (i * phaseStep);
             dxf.Entities.Add(new netDxf.Entities.Line(new Vector2(cx, yStart), new Vector2(cx, yStart - length)) { Layer = layer });
